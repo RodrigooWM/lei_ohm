0cce164d2c93b17f34d284d449759d6bd972e3e4
unknown <rodrigoaraujo2802@gmail.com>
2024-03-04 22:10:50 +0000

    fórmulas da lei de ohm.

---
+++ ohm.js
@@ -0,0 +1,47 @@
+function correntei(){
+    let coiso = document.getElementById('I')
+    coiso.innerHTML='<strong>V/R <br> P/V <br> √(P/R)</strong>'
+    coiso.style="background-color:#39D6F6;"
+
+}
+function correnteo(){
+    let coiso = document.getElementById('I')
+    coiso.innerHTML='<strong>Corrente (I)</strong>'
+    coiso.style="background-color:#D6D7D7; "
+}
+
+function tensaoi(){
+    let coiso = document.getElementById('V')
+    coiso.innerHTML='<strong>R.I <br> P/I <br> √(P.R)</strong>'
+    coiso.style="background-color:#39D6F6;"
+
+}
+function tensaoo(){
+    let coiso = document.getElementById('V')
+    coiso.innerHTML='<strong>Tensão (V)</strong>'
+    coiso.style="background-color:#D6D7D7;"
+}
+
+function resistenciai(){
+    let coiso = document.getElementById('R')
+    coiso.innerHTML='<strong>V/I <br> V²/P <br> P/I²</strong>'
+    coiso.style="background-color:#39D6F6;"
+
+}
+function resistenciao(){
+    let coiso = document.getElementById('R')
+    coiso.innerHTML='<strong>Resistência (R)</strong>'
+    coiso.style="background-color:#D6D7D7;"
+}
+
+function potenciai(){
+    let coiso = document.getElementById('P')
+    coiso.innerHTML='<strong>V.I <br> R.I² <br> V²/R</strong>'
+    coiso.style="background-color:#39D6F6;"
+
+}
+function potenciao(){
+    let coiso = document.getElementById('P')
+    coiso.innerHTML='<strong>Potência (P)</strong>'
+    coiso.style="background-color:#D6D7D7;"
+}
